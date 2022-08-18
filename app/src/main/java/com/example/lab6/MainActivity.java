58aa3f9b3fc44221f7f97c369e5495aebad5ad19
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-18 19:28:24 -0400

    done23

--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         IntentFilter filter = new IntentFilter("com.example.lab6.new_location");
         registerReceiver(br, filter);
+
     }
 
     public void updateLocation(View v){
--- app/src/main/java/com/example/lab6/aService.java
+++ app/src/main/java/com/example/lab6/aService.java
@@ -125,7 +125,6 @@ public class aService extends Service {
 
     public Location getLocation() {
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
             return null;
         }
         try{
